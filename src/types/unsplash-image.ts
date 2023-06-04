467175cd65572ccf0ba5c1885c065326a87d307e
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-06-04 11:53:11 +0400

    unsplash api & fetchin&displaying images

---
+++ src/types/unsplash-image.ts
@@ -0,0 +1,12 @@
+export interface IUnsplashImage {
+  data: any;
+  description: string;
+  user: {
+    username: string;
+  };
+  urls: {
+    raw: string;
+  };
+  width: number;
+  height: number;
+}
